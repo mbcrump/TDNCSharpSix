b504f48218a00eacb5d8a16532112fd0f432fa2e
Michael Crump <mbcrump29@gmail.com>
2014-12-03 17:10:42 -0600

    Adding another sample

--- TDNCSharpSix/Program.cs
+++ TDNCSharpSix/Program.cs
@@ -13,6 +13,41 @@ namespace TDNCSharpSix
         {
             //Sample One
             WriteLine("Hello TDN!");
+            Customer customer = new Customer();
+            WriteLine(customer.customerID);
+            string combindedString = string.Join(",", customer.customerNames.ToArray());
+            WriteLine(combindedString);
+            ReadLine();
         }
     }
-}
+
+    public class Customer
+    {
+        //Sample 2
+        //public Customer()
+        //{
+        //    customerID = Guid.NewGuid();
+        //}
+
+        //public Guid customerID { get; protected set; }
+        public Guid customerID { get; } = Guid.NewGuid();
+
+        //Sample 3
+        //public Dictionary<string, string> customerNames = new Dictionary<string, string>()
+        //    {
+        //        { "Michael Jordon", "Basketball" },
+        //        { "Peyton Manning", "Football" },
+        //        { "Babe Ruth", "Baseball" }
+        //    };
+
+
+        public Dictionary<string, string> customerNames = new Dictionary<string, string>()
+        {
+            ["Michael Jordon"] = "Basketball",
+            ["Peyton Manning"] = "Football",
+            ["Babe Ruth"] = "Baseball"
+        };
+
+    }
+
+    }
