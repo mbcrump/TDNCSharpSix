1819384b32117b95b7ae974be1725ef5713a971b
Michael Crump <mbcrump29@gmail.com>
2014-12-03 15:17:23 -0600

    Initial Commit

---
+++ TDNCSharpSix/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Console;
+
+namespace TDNCSharpSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample One
+            //No more System.Console since I referenced it using the "using" statement.
+            WriteLine("Hello TDN!");
+        }
+    }
+}
